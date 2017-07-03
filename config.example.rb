ad6e1d20c1e711cdee1a0608f5c685b5534f18a8
Jeff McFadden <jeff@mcfadden.io>
2017-07-02 19:53:29 -0700

    Update example config.

--- config.example.rb
+++ config.example.rb
@@ -1,7 +1,8 @@
 BLOCKS = [:social, :messaging, :news, :reddit, :feedwrangler]
 ADDITIONAL_BLOCKS = ["55.22.44.55"]
-EDGEOS_RULE = 2
+FIREWALL_RULE_NUMBER = "2"
 DISTRACTION_ADDRESS_GROUP_NAME = "Distracting-Stuff"
-ROUTER_IP_ADDRESS = "192.168.0.1"
-SSH_KEYFILE = "~/.ssh/myrouterkey"
-SSH_USER = "bob"
+ROUTER_IP_ADDRESS = "192.168.1.1"
+SSH_KEYFILE = "~/.ssh/id_rsa_erl"
+SSH_USER = "admin"
+FIREWALL_POLICY_NAME = "ETH1_IN"
